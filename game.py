ec3a3612dd22238f112e9c1be0ed57037f5c165c
Teapare <teapare@outlook.com>
2021-01-18 18:31:24 +0500

    added Brick class (empty yet). Changed the initial location of the player. The player now saves data about its location in a separate attribute. That way, it moves the way it's intended to, even if user's machine isn't powerful enough. Also, added the actual game event cycle.

--- game.py
+++ game.py
@@ -1,25 +1,48 @@
 import pygame
 
 
+class Brick(pygame.sprite.Sprite):
+    pass
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.image = pygame.image.load('Images/YEP_Emote_Banner.png')
         self.image = pygame.transform.scale(self.image, (100, 200))
         self.rect = self.image.get_rect()
-        self.rect.x = 10
-        self.rect.y = 1400
+        self.rect.x = 0
+        self.rect.y = 700
+        self.x = 0
 
     def move(self, direction=None, time=None):
         if (direction and time) is None:
             return None
-        self.rect.x += direction * time
-        if self.rect.x < 0:
-            self.rect.x = 0
-        if self.rect.x > 1500:
-            self.rect.x = 1500
+        self.x += direction * time * 100
+        if self.x < 0:
+            self.x = 0
+        if self.x > 1500:
+            self.x = 1500
+        self.rect.x = int(self.x)
 
 
 pygame.init()
 screen = pygame.display.set_mode((1600, 900))
-
+player = Player()
+pg = pygame.sprite.Group(player)
+running = True
+fps = 60
+i = pygame.image.load('Images/YEP_Emote_Banner.png')
+timer = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    t = timer.tick(fps)
+    if pygame.key.get_pressed()[pygame.K_d]:
+        player.move(1, t / 1000)
+    if pygame.key.get_pressed()[pygame.K_a]:
+        player.move(-1, t / 1000)
+    screen.fill('black')
+    pg.draw(screen)
+    pygame.display.flip()
