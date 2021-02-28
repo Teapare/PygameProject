8c43cc5e8ec49436d753781430e7d25a5360c4d0
Teapare <teapare@outlook.com>
2021-02-28 16:51:28 +0500

    Added the pause/unpause button. Spacebar also works to pause/unpause the game. Bricks now appear according to a question, which in turn appears in the top left corner of the screen. When paused, neither the question nor the bricks are visible to avoid abusing pause mechanic.

--- game.py
+++ game.py
@@ -18,6 +18,29 @@ char_running_animation = list(map(lambda x: (pygame.image.load('Images/CharRunni
                                              x[4], x[5]),
                                   [(0, 0, 815, 769, 815 * 150 // 769, 150), (864, 0, 520, 769, 520 * 150 // 769, 150),
                                    (1379, 0, 691, 769, 691 * 150 // 769, 150)]))
+pause_btn = list(map(lambda im: pygame.transform.scale(im, (150, 150)),
+                     (pygame.image.load('Images/pause.png'), pygame.image.load('Images/unpause.png'))))
+pause_btn = {True: pause_btn[1], False:pause_btn[0]}
+
+
+def pg_get_click(pos):
+    x = pos[0]
+    y = pos[1]
+    if not (playground_point_zero[0] <= x <= playground_point_zero[0] + playground.get_width()):
+        return False
+    if not (playground_point_zero[1] <= y <= playground_point_zero[0] + playground.get_height()):
+        return False
+    return True
+
+
+def pause_get_click(pos):
+    x = pos[0]
+    y = pos[1]
+    if not (screen_size[0] - 200 <= x <= screen_size[0]):
+        return False
+    if not (screen_size[1] - 200 <= y <= screen_size[1]):
+        return False
+    return True
 
 
 def attempt_obstacle_spawn(t):
@@ -25,20 +48,23 @@ def attempt_obstacle_spawn(t):
     brick_spawn_counter += t
     if brick_spawn_counter > BRICK_SPAWN_INTERVAL:
         brick_spawn_counter %= BRICK_SPAWN_INTERVAL
-        Obstacle(playground, )
+        Obstacle(playground, 1, 3, ["YEP"], ['DAWN', 'DAWG', 'REEEE'], _question="hello")
 
 
 class Brick(pygame.sprite.Sprite):
     image = pygame.image.load('Images/BrickLongRound.png')
 
-    def __init__(self, x, y, impassable=True, group: pygame.sprite.Group = None):
-        if impassable:
+    def __init__(self, x, y, passable=False, p_text='', group: pygame.sprite.Group = None):
+        if not passable:
             super(Brick, self).__init__(bricks)
+            self.image = pygame.image.load('Images/BrickLongRound.png')
+            self.image.blit(pygame.transform.scale(pygame.font.SysFont(
+                'comic sans', 20).render(p_text, True, pygame.color.Color('white')), (200, 124)), (0, 0))
         else:
             super(Brick, self).__init__(p_bricks)
             self.image = pygame.image.load('Images/BrickLongRound.png')
             self.image.blit(pygame.transform.scale(pygame.font.SysFont(
-                'comic sans', 20).render('YEP', True, pygame.color.Color('white')), (200, 124)), (0, 0))
+                'comic sans', 20).render(p_text, True, pygame.color.Color('white')), (200, 124)), (0, 0))
         if group:
             group.add(self)
         self.x = x
@@ -77,13 +103,24 @@ class Brick(pygame.sprite.Sprite):
 
 class Obstacle:
     def __init__(self, surface: pygame.surface.Surface, correct: int = 0, incorrect: int = 0,
-                 correct_answers=None, incorrect_answers: list = None):
+                 correct_answers=None, incorrect_answers: list = None, _question=''):
         if correct_answers is None:
             correct_answers = []
         if incorrect_answers is None:
             incorrect_answers = []
         bricks_amount = surface.get_width() // 200 + (1 if surface.get_width() % 200 != 0 else 0)
         correct_answers = [correct_answers.pop(random.randrange(len(correct_answers))) for i in range(correct)]
+        self.bricks = random.sample(correct_answers, min(len(correct_answers), bricks_amount))
+        self.bricks = [(elem, True) for elem in self.bricks]
+        ia = random.sample(incorrect_answers, min(bricks_amount - len(self.bricks), len(incorrect_answers)))
+        self.bricks += [(elem, False) for elem in ia]
+        self.bricks += [('', False) for i in range(bricks_amount - len(self.bricks))]
+        random.shuffle(self.bricks)
+        _bricks = []
+        for i, brick in enumerate(self.bricks):
+            _bricks.append(Brick(i * 200 - (surface.get_width() % 200) / 2, -124, passable=brick[1], p_text=brick[0]))
+        global question
+        question = _question
 
 
 class Player(pygame.sprite.Sprite):
@@ -127,7 +164,6 @@ class Player(pygame.sprite.Sprite):
             # self.moving_index = 0
             self.image = char_running_animation[self.moving_index][0]
             self.image = pygame.transform.scale(self.image, char_running_animation[self.moving_index][1:])
-        print(self.image.get_width() * 150 // self.image.get_height())
         if self.orientation < 0:
             self.image = pygame.transform.flip(self.image, True, False)
         self.mask = pygame.mask.from_surface(self.image)
@@ -179,39 +215,54 @@ fps = 100
 bricks = pygame.sprite.Group()
 p_bricks = pygame.sprite.Group()
 pg = pygame.sprite.Group(player)
+question = ''
 while True:
     score = 0
     running = True
+    paused = False
     player.x = playground_point_zero[0] + PLAYGROUND_SIZE[0] // 2 - player.rect.width // 2
     timer = pygame.time.Clock()
     bricks.remove(*bricks.sprites())
     p_bricks.remove(*p_bricks.sprites())
     brick_spawn_counter = 3000
+    score_display = pygame.transform.scale(font.render(str(int(score)), False, WHITE),
+                                           (67 * len(str(int(score))), 120))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                paused = not paused
             if event.type == pygame.MOUSEBUTTONDOWN:
-                Brick(event.pos[0] - playground_point_zero[0],
-                      event.pos[1] - playground_point_zero[1])
+                if pg_get_click(event.pos):
+                    Brick(event.pos[0] - playground_point_zero[0],
+                          event.pos[1] - playground_point_zero[1])
+                if pause_get_click(event.pos):
+                    paused = not paused
         t = timer.tick(fps)
-        if pygame.key.get_pressed()[pygame.K_d]:
-            player.move(1)
-        if pygame.key.get_pressed()[pygame.K_a]:
-            player.move(-1)
-        bricks.update()
-        p_bricks.update()
-        score += t / 100
-        attempt_obstacle_spawn(t)
-        player.update(t / 1000)
-        score_display = pygame.transform.scale(font.render(str(int(score)), False, pygame.color.Color(WHITE)),
-                                               (67 * len(str(int(score))), 120))
         screen.fill(BLACK)
+        if not paused:
+            if pygame.key.get_pressed()[pygame.K_d]:
+                player.move(1)
+            if pygame.key.get_pressed()[pygame.K_a]:
+                player.move(-1)
+            bricks.update()
+            p_bricks.update()
+            score += t / 100
+            attempt_obstacle_spawn(t)
+            player.update(t / 1000)
+            score_display = pygame.transform.scale(font.render(str(int(score)), False, WHITE),
+                                                   (67 * len(str(int(score))), 120))
+            question_display = pygame.transform.scale(font.render(question, False, WHITE),
+                                                      (67 * len(question), 120))
+            screen.blit(question_display, (0, 0))
         playground.fill(WHITE)
         screen.blit(score_display, (screen.get_width() - score_display.get_width(), 0))
         pg.draw(playground)
-        bricks.draw(playground)
-        p_bricks.draw(playground)
+        if not paused:
+            bricks.draw(playground)
+            p_bricks.draw(playground)
+        screen.blit(pause_btn[paused], (screen_size[0] - 200, screen_size[1] - 200))
         screen.blit(playground, playground_point_zero)
         pygame.display.flip()
